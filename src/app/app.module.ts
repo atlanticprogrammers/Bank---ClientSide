5481c646ac290aa109a561f9e91755783108d05c
ksasinduanuradha.ka <ksasinduanuradha.ka@Wabtec.com>
2021-02-09 10:51:53 +0530

    SA-cuurencyExchange-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,25 @@ import { BankInterestService } from './shared/services/bank-interest.service';
 import { SharedModule } from './shared/shared.module';
 import { EntityModule } from './entity/entity.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadderComponent } from './headder/headder.component';
+
+// import { NavbarSample1Component } from "./navbar-sample-1/navbar-sample-1.component";
+
+
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeadderComponent,
+    // DoubleNavbarModule,
+    // SidenavModule
   ],
   imports: [
     BrowserModule,
@@ -20,6 +34,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     EntityModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule
+
   ],
   providers: [BankInterestService],
   bootstrap: [AppComponent]
---
+++ src/app/headder/headder.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headder',
+  templateUrl: './headder.component.html',
+  styleUrls: ['./headder.component.scss']
+})
+export class HeadderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+// https://stackblitz.com/edit/navbar-stackoverflow-bs79mv?file=styles.scss
+// https://material.angularjs.org/1.1.1/demo/navBar
+// https://www.techiediaries.com/angular-material-navigation-toolbar-sidenav/
